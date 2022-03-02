8278dc9c965341a01c288d9b2ad8791cbb7f228a
Guilherme Monteiro <guilherme.monteiro@mobile2you.com.br>
2022-03-02 19:06:33 -0300

    chore(BUMP)

---
+++ Analysis results/Security/CryptoGuardOutput/json_cryptoguard_reader.py
@@ -0,0 +1,19 @@
+import json
+import os
+import fnmatch
+
+
+writer = open("security_cryptoguard.csv", "a")
+files = fnmatch.filter(os.listdir(os.getcwd()), '*.json')
+writer.write("apk,issues\n")
+
+for file in files:
+  f = open(file)
+  data = json.load(f)
+  writer.write(data["Target"]["FullPath"][:-1])
+  writer.write(",")
+  writer.write(str(len(data["Issues"])))
+  writer.write("\n")
+  f.close()
+
+writer.close()
