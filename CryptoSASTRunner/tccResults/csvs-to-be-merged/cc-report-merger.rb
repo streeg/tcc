a9d1e268c98b66412612d2b78e4694dc73b1562d
Guilherme Monteiro <guilherme.monteiro@mobile2you.com.br>
2023-09-19 14:41:25 -0300

    feat(TCC):(Adding parsed csv files with externalLibrary identifier and 2 new ruby scripts to do this job)

---
+++ CryptoSASTRunner/tccResults/csvs-to-be-merged/cc-report-merger.rb
@@ -0,0 +1,56 @@
+require 'csv'
+require 'json'
+require 'byebug'
+
+# Caminhos das pastas
+merged_results_path = '/home/guileb/tcc/CryptoSASTRunner/tccResults/csvs-to-be-merged/all-merged-results-cc'
+report_output_path = '/home/guileb/tcc/CryptoSASTRunner/tccResults/csvs-to-be-merged/cc-report-output'
+report_new_output_path = '/home/guileb/tcc/CryptoSASTRunner/tccResults/csvs-to-be-merged/cc-report-with-external'
+
+# Listar os arquivos nas pastas
+merged_files = Dir.entries(merged_results_path).select { |f| File.file?("#{merged_results_path}/#{f}") }
+report_files = Dir.entries(report_output_path).select { |f| File.file?("#{report_output_path}/#{f}") }
+
+# Filtrar apenas os arquivos CSV
+merged_files = merged_files.select { |f| f.end_with?('.json') }
+report_files = report_files.select { |f| f.end_with?('.apk-report.csv') }
+
+merged_files.each do |file_name|
+  base_name = File.basename(file_name.gsub('.scout.merged', ''), '.json')
+  next unless report_files.include?("#{base_name}.apk-report.csv")
+
+  quote_chars = %w[" | ~ ^ & *]
+  begin
+    report_csv = CSV.read("#{report_output_path}/#{base_name}.apk-report.csv", headers: true,
+                                                                               quote_char: quote_chars.shift,
+                                                                               col_sep: ';') # Definindo o separador como ;
+  rescue CSV::MalformedCSVError
+    quote_chars.empty? ? raise : retry
+  end
+
+  json_data = JSON.parse(File.read("#{merged_results_path}/#{file_name}"))
+
+  CSV.open("#{report_new_output_path}/#{base_name}.apk-report-extended.csv", 'w', col_sep: ';', quote_char: '',
+                                                                                  force_quotes: false) do |csv|
+    csv << report_csv.headers + ['ExternalLibrary']
+
+    report_csv.each do |row|
+      class_name = row['Class']
+      method_name = row['Method']
+      error_type = row['ErrorType']
+      violated_rule = row['ViolatedRule']
+      object = row['Object']
+      statement = row['Statement']
+
+      external_library = json_data['runs'][0]['results'].any? do |result|
+        result['locations'][0]['physicalLocation']['fileLocation']['uri']&.include?("#{class_name}.") && result['locations'][0]['physicalLocation']['fileLocation']['externalLibrary'].eql?(true)
+      end
+
+      # Evitando quebras de formatação
+      object = object.gsub(';', '/') if object
+
+      csv << [error_type, class_name, method_name, violated_rule, object, statement,
+              external_library ? 'true' : 'false']
+    end
+  end
+end
---
+++ CryptoSASTRunner/tccResults/csvs-to-be-merged/cg-report-merger.rb
@@ -0,0 +1,50 @@
+require 'csv'
+require 'json'
+require 'byebug'
+
+# Caminhos das pastas
+merged_results_path = '/home/guileb/tcc/CryptoSASTRunner/tccResults/csvs-to-be-merged/all-merged-results-cg'
+report_output_path = '/home/guileb/tcc/CryptoSASTRunner/tccResults/csvs-to-be-merged/cg-report-output'
+report_new_output_path = '/home/guileb/tcc/CryptoSASTRunner/tccResults/csvs-to-be-merged/cg-report-with-external'
+
+# Listar os arquivos nas pastas
+merged_files = Dir.entries(merged_results_path).select { |f| File.file?("#{merged_results_path}/#{f}") }
+report_files = Dir.entries(report_output_path).select { |f| File.file?("#{report_output_path}/#{f}") }
+
+# Filtrar apenas os arquivos CSV
+merged_files = merged_files.select { |f| f.end_with?('.json') }
+report_files = report_files.select { |f| f.end_with?('.apk.csv') }
+
+merged_files.each do |file_name|
+  base_name = File.basename(file_name.gsub('.scout.merged', ''), '.json')
+  # Verificar se o arquivo correspondente existe na pasta de relatórios
+  next unless report_files.include?("#{base_name}.apk.csv")
+
+  # Ler o CSV do relatório
+  report_csv = CSV.read("#{report_output_path}/#{base_name}.apk.csv", headers: true)
+
+  # Ler o JSON de resultados
+  json_data = JSON.parse(File.read("#{merged_results_path}/#{file_name}"))
+
+  # Processar os dados e criar novo CSV
+  CSV.open("#{report_new_output_path}/#{base_name}.apk-extended.csv", 'w') do |csv|
+    csv << report_csv.headers + ['ExternalLibrary']
+
+    report_csv.each do |row|
+      class_name = row['ClassName']
+      method_name = row['MethodName']
+
+      # Verificar se a classe existe no JSON e se é uma biblioteca externa
+      external_library = if json_data['Issues'].any? do |issue|
+                              issue['_FullPath'].include?("#{class_name}.class") && issue['externalLibrary'] == true
+                            end
+                           true
+                         else
+                           false
+                         end
+
+      # Escrever no novo CSV
+      csv << row.fields + [external_library]
+    end
+  end
+end
